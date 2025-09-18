9fb38f3168060d55e15038ef73a66bbc4f65edd6
Zach Hoagland <zahoagland@gmail.com>
2025-09-18 18:42:20 -0400

    Working on linked_list.c and linked_list.h

---
+++ src/include/linked_list.h
@@ -0,0 +1,45 @@
+/**
+ * @file linked_list.h
+ * @author Zachary Hoagland (zach@zacharyhoagland.com)
+ * @brief API for the linked list data structure library.
+ * @version 1.0
+ * @date 2025-04-03
+ *
+ * @copyright Copyright Zachary Hoagland (c) 2025
+ *
+ */
+#ifndef _DSA_LINKED_LIST_H_
+#define _DSA_LINKED_LIST_H_
+
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+
+/* -------------------- Public Includes --------------------------------- */
+
+
+/* -------------------- Public Macros/Defines --------------------------- */
+
+
+/* -------------------- Public Enums ------------------------------------ */
+
+
+/* -------------------- Public Structs ---------------------------------- */
+typedef dsa_linked_list_h LinkedListHandle;
+
+/* -------------------- Public (global) Vars ---------------------------- */
+
+
+/* -------------------- Public Function Declarations -------------------- */
+
+
+
+
+#ifdef __cplusplus
+    }
+#endif
+
+
+#endif /* _DSA_LINKED_LIST_H_ */
